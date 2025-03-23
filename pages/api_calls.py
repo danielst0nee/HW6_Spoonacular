e7ceb0f69c7d1f8a2872178d61fe49c3768cefe6
Daniel Stone <stoned12@my.erau.edu>
2025-03-22 17:32:53 -0700

    Finished Random Recipes

--- pages/api_calls.py
+++ pages/api_calls.py
@@ -5,31 +5,63 @@ Filename: api_calls.py
 
 File Description: Functions for the Spoonacular API calls
 """
+import os
+from json import dump, load
+from datetime import timedelta
 from requests import get
 import streamlit as st
 
+
 API_KEY = st.secrets["API_key"]
+RECIPE_DATA_FILE = "data/recipe_data.json"
 
 
-def get_random_recipe():
+@st.cache_data(show_spinner="Finding delicious recpipes...", ttl=timedelta(days=1))
+def get_random_recipe(min_health_score: int = 0, num_recipes: int = 1) -> dict:
     """
     Gets a random recipe from Spoonacular API
     """
-    try:
-        response = get(f"https://api.spoonacular.com/recipes/random?apiKey={API_KEY}&number=", timeout=3)
-    except OSError:
-        print("Request Failed")
+    data = None
 
-    if response.status_code == 200:
-        data = response.json()
-        recipes = data["recipes"][0]
-        recipe_info = {
-            "title": recipes["title"],
-            "image": recipes["image"]
-        }
-        return recipe_info
-    else:
-        print("it didn't work")
+    # Trying to load from JSON file first
+    if os.path.exists(RECIPE_DATA_FILE):
+        try:
+            with open(RECIPE_DATA_FILE, "r", encoding="utf-8") as file:
+                data = load(file)
+                recipes = data["recipes"]
+                if recipes:
+                    print("Recipes loaded from backup file")
+        except (OSError, ValueError):
+            print("Initiating API Request...")
+
+    if not data:
+        try:
+            response = get(f"https://api.spoonacular.com/recipes/random?apiKey={API_KEY}&number=100", timeout=3)
+
+            if response.status_code == 200:
+                data = response.json()
+                with open(RECIPE_DATA_FILE, "w", encoding="utf-8") as file:
+                    dump(data, file)
+                recipes = data["recipes"]
+        except Exception as e:
+            print(f"API request failed: {e}")
+            return []
+
+    recipe_info = []
+    for recipe in recipes:
+        if recipe["healthScore"] >= min_health_score:
+            recipe_info.append({
+                "health_score": recipe["healthScore"],
+                "title": recipe["title"],
+                "image": recipe["image"],
+                "time": recipe["readyInMinutes"],
+                "servings": recipe["servings"],
+                "link": recipe["sourceUrl"],
+                "ingredients": [ingredient["original"] for ingredient in recipe["extendedIngredients"]]
+            })
+        if len(recipe_info) >= num_recipes:
+            break
+    return recipe_info
 
 
 get_random_recipe()
--- pages/random_recipe.py
+++ pages/random_recipe.py
@@ -10,6 +10,24 @@ from api_calls import get_random_recipe
 
 st.title("Random Recipe Generator")
 
-recipe = get_random_recipe()
-st.header(recipe["title"])
-st.image(recipe["image"])
+num_recipes = st.slider("Number of Recipes", 1, 5, 1)
+min_health_score = st.slider("Minimum Health Score", 0, 100, 0)
+
+recipes = get_random_recipe(min_health_score, num_recipes)
+
+if len(recipes) < num_recipes:
+    st.warning(f"Only {len(recipes)} recipes with that health score could be found. Please try again.")
+
+for recipe in recipes:
+    with st.container(border=True):
+        st.header(recipe["title"])
+        st.image(recipe["image"])
+
+        st.subheader("Recipe Information:")
+        st.write(f"Time: {recipe["time"]} minutes")
+        st.write(f"Servings: {recipe["servings"]}")
+        st.write(f"Health Score: {recipe["health_score"]}")
+        st.write(f"Link: {recipe["link"]}")
+
+        st.subheader("Ingredients:")
+        st.markdown("\n".join([f" - {ingredient}" for ingredient in recipe["ingredients"]]))
