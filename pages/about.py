62b5ea998ef20109f7bdaf206ab926e582d97759
Daniel Stone <stoned12@my.erau.edu>
2025-03-22 17:52:54 -0700

    Finished About page

--- pages/about.py
+++ pages/about.py
@@ -7,4 +7,15 @@ File Description: Second page that provides a description of the web application
 """
 import streamlit as st
 
-st.title("About this application")
+st.title("About This App")
+st.write("""
+         This app helps you find healthy and delicious recipes using the Spoonacular API.
+         You can filter recipes by health score and get recommendations.
+         """)
+
+st.subheader("How It Works")
+st.write("""
+- Select your preferred health score range.
+- Choose how many recipes you want.
+- See the recipes appear!
+""")
